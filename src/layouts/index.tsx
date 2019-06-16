5bdd5cde90cdfac870a6301dc6cf2f4e1e200fbd
Matt Downs <mattdowns6@gmail.com>
2019-06-16 16:13:58 +1000

    Add bootstrap and sass plugins

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import Link from "gatsby-link";
 import Helmet from "react-helmet";
 
-import "./index.css";
+import "./index.scss";
 
 const Header = () => (
   <div
