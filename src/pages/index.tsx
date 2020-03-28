999922a2988ad72ed20e339b4de765ced3175d5a
Matt Downs <mattdowns6@gmail.com>
2020-03-28 11:35:15 +1000

    Update byline

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ const Index = () => {
             <div className="col-md-8">
               <p className="lead mb-0">
                 A full stack web developer with a passion for using modern
-                technologies to provide a great end user experience.
+                technologies to provide great end user experiences.
               </p>
               <div className="only-print">
                 <p className="small mt-3 mb-0">
